49d9864d1f03bd02fccac20c7f3144482992206e
srkayrez <srkayrez@gmail.com>
2023-08-29 21:10:22 -0300

    add status bar

--- Screens/superior.js
+++ Screens/superior.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable, ScrollView, Alert, } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView, Alert, Pressable, } from 'react-native';
 import * as React from 'react';
 import { Switch, Card } from 'react-native-paper';
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -7,14 +7,19 @@ import { Feather } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import { List } from 'react-native-paper';
 import { Treinos } from './exercicio';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, RefreshControl } from 'react-native-gesture-handler';
+import { ProgressBar, MD3Colors } from 'react-native-paper';
+import {useState} from 'react';
+import { Checkbox } from 'react-native-paper';
+import { ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
 
 export function Superior() {
   
-
-
+  const [timesPressed, setTimesPressed] = useState(0);
+  const [checked, setChecked] = React.useState(false);
+  
   return (
-  <ScrollView>
+    <ScrollView>
 
     <View style={styles.container}>
      
@@ -29,16 +34,38 @@ export function Superior() {
         {dados.map((dados) => {
           return (
             
-          
-          <View style={{alignContent:'center', alignItems:'baseline', marginBottom:15}}>
+            
+            <View style={{alignContent:'center', alignItems:'baseline', marginBottom:15}}>
             <Card style={{alignContent:'center', width:350, borderRadius:10,}}>
               <Card.Content style={{}}>
+              <ProgressBar progress={numeroSerieConcluida} style={{backgroundColor:'#EDCAFF', color:'#693E7F'}} />
+              <View>
                 <Text style={{textAlign:'center', fontSize:20, marginBottom:10}}> {dados.exercicio} </Text>
+                <Checkbox
+                  status={checked ? 'checked' : 'unchecked'}
+                  onPress={() => { 
+                    setChecked(!checked);
+                    soma();
+                    
+                    
+                  }}
+                  />
+                    <Button
+                    onPress={soma}
+                    title="Learn More"/>
+              </View>
                 <Image source={require('../assets/imagens/braco.png')} style={{width:100, height:100, marginBottom:10, alignSelf:'center'}}></Image>
                 <View style={{marginLeft:20, flexDirection:'row', alignSelf:'center', gap:15}}>
                   <Text style={styles.descricao}><Feather name="repeat" size={15} color="#EDCAFF" />  {dados.serie} / {dados.repeticao} </Text>
                   <Text style={styles.descricao}><MaterialCommunityIcons name="weight-kilogram" size={20} color="#EDCAFF" />  {dados.peso} kg </Text>
                   <Text style={styles.descricao}><MaterialCommunityIcons name="clock-time-eight-outline" size={20} color="#EDCAFF" />  {dados.descanco}s </Text>
+                </View>
+                <View>
+
+                  <Pressable onPress={soma}>
+                    <Text>teste {numeroSerieConcluida}</Text>
+                  </Pressable>
+                  
                 </View>
               </Card.Content>
             </Card>
@@ -50,22 +77,22 @@ export function Superior() {
         </ScrollView>
     );
   }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#B0B0B0',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%'
-  },
-  btnSelectTraining: {
-    backgroundColor: 'white',
-    alignItems: 'center',
-    borderRadius: 5,
-    borderColor: 'black',
-    width:'100%',
-    marginVertical:20,
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#B0B0B0',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%'
+    },
+    btnSelectTraining: {
+      backgroundColor: 'white',
+      alignItems: 'center',
+      borderRadius: 5,
+      borderColor: 'black',
+      width:'100%',
+      marginVertical:20,
     top:0,
   },
   descricao: {
@@ -76,29 +103,46 @@ const styles = StyleSheet.create({
     paddingVertical:5,
     textAlign:'center',
   }
-    
+  
 });
 
 const dados = [{
+  id: 1,
   exercicio: 'biceps halter',
   serie: 4,
   repeticao: 10,
   peso: 25,
-  descanco: 60
+  descanco: 60,
+  seriesFeitas : 0
 },
 {
+  id: 1,
   exercicio: 'biceps barra',
   serie: 4,
   repeticao: 10,
-  peso: 30,
-  descanco: 60
+  peso: 30,                                      
+  descanco: 60,
+  seriesFeitas : 0
 },
 {
+  id: 1,
   exercicio: 'biceps rosca',
   serie: 4,
   repeticao: 10,
   peso: 30,
-  descanco: 60
+  descanco: 60,
+  seriesFeitas : 0
 },
-]
+];
 
+// const [numeroSerieConcluida ] = useState(0);
+
+var numeroSerieConcluida = 0.0
+function soma(){
+ if (numeroSerieConcluida < 1){
+   numeroSerieConcluida += 0.25;
+
+ }else numeroSerieConcluida = 0;
+
+  
+}
